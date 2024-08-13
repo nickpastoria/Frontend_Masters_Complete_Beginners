8c1155cefb07d3fe74f11c9e33dff15b95e73afb
nickpastoria <nickpastoria@gmail.com>
2024-08-13 10:22:27 -0700

    created and imported scratch.js into notes

---
+++ scratch/experiments.js
@@ -0,0 +1,13 @@
+// scratch/experiments.js
+console.log("Hello World");
+
+// Small Looping Example
+// Should be pretty easy
+
+const timesToRepeat = 10;
+const word = "String Beans";
+
+// Forgot that for loops in js are closer to c++
+for (let i = 0; i < timesToRepeat; i++) {
+  console.log(word);
+}
